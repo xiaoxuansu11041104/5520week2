ee2aa088b76fbd5572268433522dcf268d56464d
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-09-12 13:02:10 -0700

    Update input

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
-import React, { useState } from 'react';
+import Input from './Components/Input';
+
 
 export default function App() {
 
   // Declare a constant variable for the app name
   const appName = "My awesome app";
-  const [text, setText] = useState("");
 
-  function updateText(ChangeText) {
-    setText(ChangeText);
-  }
+
+
   
   return (
     <View style={styles.container}>
       
       <StatusBar style="auto" />
       <Header name={appName} />
-        <TextInput 
-          placeholder="Type something"
-          keyboardType="default"
-          style={{ borderBottomColor: 'purple', borderBottomWidth: 1, width: 200, marginBottom: 10 }}
-          value={text}
-          onChangeText={updateText}
-        />
-        <Text>{text}</Text>
-        
- 
+      <Input />
     </View>
   );
 }
