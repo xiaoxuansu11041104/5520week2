c834f1416ed90951ccec18997f356fd2667969ab
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-09-12 11:32:42 -0700

    activity variable

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // Declare a constant variable for the app name
+  const appName = "My awesome app";
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
