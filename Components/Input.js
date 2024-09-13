e39e051152664783c83b376fa483df39088b75cf
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-09-12 18:57:07 -0700

    Added autoFocus prop to control TextInput focus on render

--- Components/Input.js
+++ Components/Input.js
@@ -1,15 +1,25 @@
 import { Text, View, TextInput } from 'react-native'
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-export default function Input() {
+export default function Input({autoFocus}) {
     const [text, setText] = useState('');
+    const textInputRef = useRef(null); // Reference to manage focus
 
-    function updateText(ChangeText) {
-        setText(ChangeText);
+    // Set focus on the TextInput when the component mounts if autoFocus is true
+    useEffect(() => {
+    if (autoFocus && textInputRef.current) {
+        textInputRef.current.focus(); // Automatically focus the input if autoFocus is true
+    }
+    }, [autoFocus]);
+
+
+    function updateText(newText) {
+        setText(newText);
     }
   return (
     <View>
-        <TextInput 
+        <TextInput
+          ref={textInputRef} 
           placeholder="Type something"
           keyboardType="default"
           style={{ borderBottomColor: 'purple', borderBottomWidth: 1, width: 200, marginBottom: 10 }}
