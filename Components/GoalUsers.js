56881d17d64d1bd914c761d530c7ed07d3c689bf
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-10-24 11:59:45 -0700

    Render User

--- Components/GoalUsers.js
+++ Components/GoalUsers.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 import { useEffect, useState } from 'react'
+
 
 export default function GoalUsers() {
   const [users, setUsers] = useState([])
@@ -11,13 +12,22 @@ export default function GoalUsers() {
           "https://jsonplaceholder.typicode.com/users/"
         );
 
+
         if (!response.ok) {
           throw new Error(
             `An HTTP error happened with status: ${response.status}`
           );
         }
+        
 
         const data = await response.json();
+        
+        // Call the json method on the response to get JSON
+        setUsers(
+          data.map((user) => {
+            return user.name;
+          })
+        );
         // set the users state variable from the data
       } catch (err) {
         console.log("fetch user data ", err);
@@ -30,7 +40,12 @@ export default function GoalUsers() {
 
   return (
     <View>
-      <Text>GoalUsers</Text>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => {
+          return <Text>{item}</Text>;
+        }}
+      />
     </View>
   )
 }
