280688d0e54fb5933f9b40237d03813bf865ffd9
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-10-31 12:32:03 -0700

    Profile page

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ import GoalDetails from "./Components/GoalDetails";
 import { Button } from "react-native";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import { getHeaderOptions } from "./Components/HeaderOptions";
+import { useState, useEffect } from "react";
 
 const Stack = createNativeStackNavigator();
-import { getHeaderOptions } from "./Components/HeaderOptions";
+const authStack = (
+  <>
+    <Stack.Screen name="Signup" component={Signup} />
+    <Stack.Screen name="Login" component={Login} />
+  </>
+);
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen
+const appStack = (
+  <>
+    <Stack.Screen
           name="Home"
           component={Home}
           options={
@@ -28,22 +31,48 @@ export default function App() {
           component={GoalDetails}
           options={({ navigation, route }) => {
             return {
-              ...getHeaderOptions(route.params ? route.params.goalObj.text : "More Details"),
-              headerRight: () => {
-                return (
-                  <Button
-                    title="Warning"
-                    onPress={() => {
-                      console.log("warning");
-                    }}
-                  />
-                );
-              },
+              title: route.params ? route.params.goalData.text : "More Details",
+              // headerRight: () => {
+              //   return (
+              //     <Button
+              //       title="Warning"
+              //       onPress={() => {
+              //         console.log("warning");
+              //       }}
+              //     />
+              //   );
+              // },
             };
           }}
         />
-        
+  </>
+);
+
+
+export default function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsUserLoggedIn(true);
+      } else {
+        setIsUserLoggedIn(false);
+      }
+      //based on the user variable, set the state variable isUserLoggedIn
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+        <Stack.Navigator
+        // initialRouteName="Login"
+        screenOptions={{
+          headerStyle: { backgroundColor: "purple" },
+          headerTintColor: "white",
+        }}
+      >     
       </Stack.Navigator>
+      {isUserLoggedIn ? appStack : authStack}
     </NavigationContainer>
   );
 }
