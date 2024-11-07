4b97e0efef369701a1d4e70ec0c4ef12e3478656
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-11-07 11:41:38 -0800

    Register permission

--- Components/ImageManager.js
+++ Components/ImageManager.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import * as ImagePicker from 'expo-image-picker'
 
 export default function ImageManager() {
+
+  function takeImageHandler() {
+    ImagePicker
+  }
+
   return (
     <View>
       <Text>ImageManager</Text>
